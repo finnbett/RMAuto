4402bb5db04dce1c1c0b726257805eddfe950301
finnbett <85616388+finnbett@users.noreply.github.com>
2021-12-21 21:41:50 +1300

    wire frame and logo

---
+++ src/Header.jsx
@@ -0,0 +1,1 @@
+//logo with the other contact information
---
+++ src/Services.jsx
@@ -0,0 +1,9 @@
+//Servicing, Suspension, Brakes, Clutches, Tune Ups, Exhaust, Panel and Paint, ECU scan, Electrical,
+//General Mechanical,Tyres, Rust Repair, Cambelt, Batteries.
+
+
+export function Services () {
+    return{
+        //list of services
+    }
+}
