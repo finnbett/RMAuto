e66a18bc028fba0df7629c11c85a6dd43ef5682b
finnbett <85616388+finnbett@users.noreply.github.com>
2021-12-24 11:29:58 +1300

    icons and nav

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,17 +1,24 @@
 // map, facebook page, phone, email, address, opening hours.
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
 
 export function Contact () {
     return( 
     <>
         <h1>Contact</h1>
         <h2>Phone</h2>
+        <FontAwesomeIcon icon={faPhone} className="icon"></FontAwesomeIcon><span>022 192 9380</span>
         <h2>Facebook</h2>
+                <FontAwesomeIcon icon={faFacebook} className="icon"></FontAwesomeIcon>
         <h2>Email</h2>
+                <FontAwesomeIcon icon={faEnvelope} className="icon"></FontAwesomeIcon><span>rikomahe18@gmail.com</span>
         <h2>Opening Hours</h2>
         <h2>Address</h2>
-        <p>4 Hautonga Street, Petone, Lower Hutt</p>
+        <p>4 Hautonga Street <br/> Petone,<br/> Lower Hutt</p>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d750.1211603393833!2d174.89341332926176!3d-41.23299999870492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38abcbf78e3407%3A0xf7d5ffb2ddc4f983!2s4%20Hautonga%20Street%2C%20Petone%2C%20Lower%20Hutt%205012!5e0!3m2!1sen!2snz!4v1640122456321!5m2!1sen!2snz" title="Location" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d750.1211603393833!2d174.89341332926176!3d-41.23299999870492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38abcbf78e3407%3A0xf7d5ffb2ddc4f983!2s4%20Hautonga%20Street%2C%20Petone%2C%20Lower%20Hutt%205012!5e0!3m2!1sen!2snz!4v1640122456321!5m2!1sen!2snz" title="Location" width="600" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy"></iframe>
     </>
     )
     //map
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,32 @@
 // logo with the other contact information
 //font highspeed regular, goldman
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
 
 export function Header () {
-    return (<>
-        <img src="./rmautosjpg3.png" alt="logo" />
+    return (
+        <>
+            <div className="navbar">
+                <img src="./rmlogo.png" alt="logo" />
+                <h1 className="title">R.M. Auto Repairs</h1>
+                <FontAwesomeIcon icon={faPhone} className="icon"></FontAwesomeIcon><span className="phone-header">022 192 9380</span>
+                <FontAwesomeIcon icon={faEnvelope} className="icon"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faFacebook} className="icon"></FontAwesomeIcon>
+
+                
+                <h2 className="link">Services</h2>
+                <h2 className="link">Contact</h2>
+
+
+            </div>
         </>
     )
+  
 }
+
+// return (<>
+//     <img src="./rmautosjpg3.png" alt="logo" />
+//     </>
+// )
