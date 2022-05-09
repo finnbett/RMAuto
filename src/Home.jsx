3921cb271c0fed8956175a2dbcc2c3acaae98de2
finnbett <85616388+finnbett@users.noreply.github.com>
2022-05-09 17:41:09 +1200

    bootstrap and apply bg images

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 // feature photo and main content.
 
+
 export function Home () {
     return(
         <>
         <div className="home-container"> 
             <h1 id="content-caption">We Repair all Makes and Models</h1>
+        
         </div>
         </>
     )
